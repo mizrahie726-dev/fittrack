4541806922475e50c2c7dd1f47029c785d7ce44b
mizrahie726-dev <mizrahie726@gmail.com>
2026-03-22 11:23:07 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE = 'fittrack-v5';
+const ASSETS = [
+  '/fittrack/',
+  '/fittrack/index.html',
+  '/fittrack/manifest.json',
+  '/fittrack/icon-192.png',
+  '/fittrack/icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res && res.status === 200) {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => caches.match('/fittrack/'));
+    })
+  );
+});
